9fc00912c61d131d43e7991a77586b170e44b6e3
wesleygdike <wesley.g.dike@gmail.com>
2017-12-22 22:12:22 -0700

    Updated css style sheet ref. 12/22/17

--- view/partial/head.php
+++ view/partial/head.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"/>
-  <link rel="stylesheet" href="myStyle.css" />
+  <link rel="stylesheet" href="/myStyle.css" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
