3f9aa36a3fd29fe46c5123a881420488f29d81b4
wesleygdike <wesley.g.dike@gmail.com>
2017-12-22 21:13:39 -0700

    update 12/22/17

---
+++ view/techimplementations.php
@@ -0,0 +1,15 @@
+<?php
+include __DIR__ . '/view/partial/head.php';
+include __DIR__ . '/view/partial/header.php';
+?>
+<!-- Second Container -->
+<div class="container-fluid bg-2 text-center">
+  <h3 class="margin">Follow Me, and see what I can do!</h3>
+    <a href="https://twitter.com/Wesley_g_dike"><img src="https://cdn1.iconfinder.com/data/icons/iconza-circle-social/64/697029-twitter-512.png" class="img-" style="width:80px" alt="Image"></a>
+    <a href="https://github.com/wesleygdike"><img src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png" class="img-" style="width:60px" alt="Image"></a>
+</div>
+
+<!-- Third Container -->
+<?php 
+include __DIR__ . '/view/partial/navbar.php';
+include __DIR__ . '/view/partial/footer.php';
