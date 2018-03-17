2c95254490401374aec2b16d093a4e26db81504b
wesleygdike <wesley.g.dike@gmail.com>
2018-03-17 13:34:51 -0600

    initial content for design prject

--- view/hidesignproject.php
+++ view/hidesignproject.php
@@ -3,13 +3,95 @@ include __DIR__ . '/../view/partial/hidp_head.php';
 include __DIR__ . '/../view/partial/header.php';
 ?>
 <!-- First Container -->
-<div class="container-fluid bg-2 text-center">
-  <h3 class="margin">Title</h3>
-    <div class="row">
-        <div class="col-sm-4" data-toggle="tooltip">
-            
-        </div>
+<body>        
+    <div data-role="main" class="ui-content">
+    <h2>I-Learn Home View</h2>
+    <div data-role="collapsible">
+    <h4>Upcoming Events</h4>
+    <ul data-role="listview">
+      <li><a class="dropdown-item" href="#">
+                      <div class="classdata">Class ## (Online): Semester Year</div>
+                        <div class="progress">
+                          <span class="progress-bar" style="width:50%">
+                          </span>
+                        </div>
+                      <div class="txtprogess">75% 3 of 4 topics complete</div>
+                  </a>  <!-- Dropdown Menu Ends -->
+      </li>
+    </ul>
+    <ul data-role="listview">
+      <li><a class="dropdown-item" href="#">
+                      <div class="classdata">Class ## (Online): Semester Year</div>
+                        <div class="progress">
+                          <span class="progress-bar" style="width:50%">
+                          </span>
+                        </div>
+                      <div class="txtprogess">75% 3 of 4 topics complete</div>
+                  </a>  <!-- Dropdown Menu Ends -->
+      </li>
+    </ul>    
     </div>
+    </div>
+    <div data-role="main" class="ui-content">
+    <h2>I-Learn Class Home View</h2>
+    <div data-role="collapsible">
+    <h4>Upcoming Events</h4>
+    <ul data-role="listview">
+      <li>                  <!-- Dropdown Item -->
+                  <a class="dropdown-item" href="#">
+                    <div class="floating-box">
+                        <div class="date "><div class="weekday">Mon</div><div class="day">30</div></div>
+                        <div class="completionstatusicon"><input type="checkbox"></div>
+                    </div>
+                    <div class="row">
+                        <span class="duetime ">5PM<span class="duealert ">-due</span></span>
+                        <span class="grade ">Grade 0/20</span>
+                    </div> 
+                    <div class="row">
+                          <span class="assignmentinfo">Assignment Type: Assignment Title</span>
+                    </div>
+                    <div class="row">
+                          <span class="assignmentdescandtimeest">Brief assignment description & Est. Time requirements</span>
+                    </div>  
+                  </a> <!-- Dropdown Item Ends --></li>
+      <li>                  <!-- Dropdown Item -->
+                  <a class="dropdown-item" href="#">
+                    <div class="floating-box">
+                        <div class="date "><div class="weekday">Mon</div><div class="day">30</div></div>
+                        <div class="completionstatusicon"><input type="checkbox"></div>
+                    </div>
+                    <div class="row">
+                        <span class="duetime ">5PM<span class="duealert ">-due</span></span>
+                        <span class="grade ">Grade 0/20</span>
+                    </div> 
+                    <div class="row">
+                          <span class="assignmentinfo">Assignment Type: Assignment Title</span>
+                    </div>
+                    <div class="row">
+                          <span class="assignmentdescandtimeest">Brief assignment description & Est. Time requirements</span>
+                    </div>  
+                  </a> <!-- Dropdown Item Ends --></li>
+      <li>                  <!-- Dropdown Item -->
+                  <a class="dropdown-item" href="#">
+                    <div class="floating-box">
+                        <div class="date "><div class="weekday">Mon</div><div class="day">30</div></div>
+                        <div class="completionstatusicon"><input type="checkbox"></div>
+                    </div>
+                    <div class="row">
+                        <span class="duetime ">5PM<span class="duealert ">-due</span></span>
+                        <span class="grade ">Grade 0/20</span>
+                    </div> 
+                    <div class="row">
+                          <span class="assignmentinfo">Assignment Type: Assignment Title</span>
+                    </div>
+                    <div class="row">
+                          <span class="assignmentdescandtimeest">Brief assignment description & Est. Time requirements</span>
+                    </div>  
+                  </a> <!-- Dropdown Item Ends --></li>
+    </ul>
+    </div>
+    </div>
+    </body>
 </div>
 <?php 
 include __DIR__ . '/../view/partial/footer.php';
