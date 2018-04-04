747af9b9094b86532ec2213fd2c9dea063cf442f
wesleygdike <wesley.g.dike@gmail.com>
2018-04-04 17:39:59 -0600

    hidesignproject

--- view/hidesignproject_1.php
+++ view/hidesignproject_1.php
@@ -5,8 +5,63 @@ include __DIR__ . '/../view/partial/header.php';
 <!-- First Container -->
 <body>        
     <div data-role="main" class="ui-content">
-    <h2>I-Learn Assignment View</h2>
-
+    <h2>I-Learn Class Home View</h2>
+    <div data-role="collapsible">
+    <h4>Upcoming Events</h4>
+    <ul data-role="listview">
+      <li>                  <!-- Dropdown Item -->
+          <a class="dropdown-item" href="hidesignproject_2.php">
+                    <div class="floating-box">
+                        <div class="date "><div class="weekday">Mon</div><div class="day">30</div></div>
+                        <div class="completionstatusicon"><input type="checkbox"></div>
+                    </div>
+                    <div class="row">
+                        <span class="duetime ">5PM<span class="duealert ">-due</span></span>
+                        <span class="grade ">Grade 0/20</span>
+                    </div> 
+                    <div class="row">
+                          <span class="assignmentinfo">Assignment Type: Assignment Title</span>
+                    </div>
+                    <div class="row">
+                          <span class="assignmentdescandtimeest">Brief assignment description & Est. Time requirements</span>
+                    </div>  
+                  </a> <!-- Dropdown Item Ends --></li>
+      <li>                  <!-- Dropdown Item -->
+          <a class="dropdown-item" href="hidesignproject_2.php">
+                    <div class="floating-box">
+                        <div class="date "><div class="weekday">Mon</div><div class="day">30</div></div>
+                        <div class="completionstatusicon"><input type="checkbox"></div>
+                    </div>
+                    <div class="row">
+                        <span class="duetime ">5PM<span class="duealert ">-due</span></span>
+                        <span class="grade ">Grade 0/20</span>
+                    </div> 
+                    <div class="row">
+                          <span class="assignmentinfo">Assignment Type: Assignment Title</span>
+                    </div>
+                    <div class="row">
+                          <span class="assignmentdescandtimeest">Brief assignment description & Est. Time requirements</span>
+                    </div>  
+                  </a> <!-- Dropdown Item Ends --></li>
+      <li>                  <!-- Dropdown Item -->
+          <a class="dropdown-item" href="hidesignproject_2.php">
+                    <div class="floating-box">
+                        <div class="date "><div class="weekday">Mon</div><div class="day">30</div></div>
+                        <div class="completionstatusicon"><input type="checkbox"></div>
+                    </div>
+                    <div class="row">
+                        <span class="duetime ">5PM<span class="duealert ">-due</span></span>
+                        <span class="grade ">Grade 0/20</span>
+                    </div> 
+                    <div class="row">
+                          <span class="assignmentinfo">Assignment Type: Assignment Title</span>
+                    </div>
+                    <div class="row">
+                          <span class="assignmentdescandtimeest">Brief assignment description & Est. Time requirements</span>
+                    </div>  
+                  </a> <!-- Dropdown Item Ends --></li>
+    </ul>
+    </div>
     </div> 
     </body>
 </div>
