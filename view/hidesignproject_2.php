62746776444e285cdcb13d7fb0ca1af20e7f3ebb
wesleygdike <wesley.g.dike@gmail.com>
2018-04-07 08:59:13 -0600

    Correction of home page link.

--- view/hidesignproject_2.php
+++ view/hidesignproject_2.php
@@ -6,7 +6,9 @@ include __DIR__ . '/../view/partial/header.php';
 <body>        
     <div data-role="main" class="ui-content">
     <h2>I-Learn Assignment View</h2>
-
+    <p>This view is not covered in this project's scope. This page acts as a
+    placeholder for the actual I-Lean assignment view.</p>
+    <a href="hidesignproject.php">Start Over</a>
     </div> 
     </body>
 </div>
--- view/partial/header.php
+++ view/partial/header.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-default">
   <div class="container">
     <div class="navbar-header">
-      <a class="navbar-brand" href="/index.php">Wesley G. Dike</a>
+      <a class="navbar-brand" href="https://wesleygdike.herokuapp.com">Wesley G. Dike</a>
     </div>
   </div>
 </nav>
